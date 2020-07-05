5cbc0592014b54de836dce9a368877ddf5bbef11
CDebayan <cdebayan09@gmail.com>
2020-07-05 22:08:06 +0530

    profile screen update about

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -4,6 +4,7 @@ import 'package:whatsappcloneflutter/blocs/user_profile_bloc/user_profile_bloc.d
 import 'package:whatsappcloneflutter/blocs/user_profile_bloc/user_profile_event.dart';
 import 'package:whatsappcloneflutter/blocs/user_profile_bloc/user_profile_state.dart';
 import 'package:whatsappcloneflutter/constants.dart';
+import 'package:whatsappcloneflutter/functionality.dart';
 import 'package:whatsappcloneflutter/widgets/widgets.dart';
 
 class AboutScreen extends StatefulWidget {
@@ -13,7 +14,7 @@ class AboutScreen extends StatefulWidget {
   _AboutScreenState createState() => _AboutScreenState();
 }
 
-class _AboutScreenState extends State<AboutScreen> {
+class _AboutScreenState extends State<AboutScreen> with Functionality {
   List<String> _aboutList = [
     "Available",
     "Busy",
@@ -34,44 +35,66 @@ class _AboutScreenState extends State<AboutScreen> {
       appBar: AppBar(
         title: Text("About"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 16, left: 24, right: 24),
-            child: Text(
-              "Currently set to",
-              style: TextStyle(color: Constants.colorPrimary),
-            ),
-          ),
-          BlocBuilder<UserProfileBloc, UserProfileState>(
-              builder: (context, state) {
-            if (state is NoInternet) {
-              return Container();
-            } else {
-              return _buildWidget(state);
-            }
-          }),
-          Divider(),
-          Padding(
-            padding: const EdgeInsets.only(top: 16, left: 24, right: 24),
-            child: Text(
-              "Select About",
-              style: TextStyle(color: Constants.colorPrimary),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-                itemCount: _aboutList.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    contentPadding: EdgeInsets.only(left: 24,right: 24),
-                    title: Text(_aboutList[index]),
-                  );
-                }),
-          ),
-        ],
-      ),
+      body: BlocBuilder<UserProfileBloc, UserProfileState>(
+          builder: (context, state) {
+        if (state is NoInternet) {
+          return Container();
+        } else {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 16, left: 24, right: 24),
+                child: Text(
+                  "Currently set to",
+                  style: TextStyle(color: Constants.colorPrimary),
+                ),
+              ),
+              _buildWidget(state),
+              Divider(),
+              Padding(
+                padding: const EdgeInsets.only(top: 16, left: 24, right: 24),
+                child: Text(
+                  "Select About",
+                  style: TextStyle(color: Constants.colorPrimary),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: _aboutList.length,
+                    itemBuilder: (context, index) {
+                      return _buildAboutItem(index, state);
+                    }),
+              ),
+            ],
+          );
+        }
+      }),
+    );
+  }
+
+  ListTile _buildAboutItem(int index, UserProfileState state) {
+    bool showSelected = false;
+
+    if (isValidObject(state.userDetails) &&
+        isValidString(state.userDetails.about)) {
+      if (state.userDetails.about == _aboutList[index]) {
+        showSelected = true;
+      }
+    }
+
+    return ListTile(
+      contentPadding: EdgeInsets.only(left: 24, right: 24),
+      title: Text(_aboutList[index]),
+      trailing: showSelected
+          ? Icon(
+              Icons.check,
+              color: Constants.colorPrimary,
+            )
+          : null,
+      onTap: (){
+        _blocInstance().add(UpdateAboutEvent(_aboutList[index]));
+      },
     );
   }
 
@@ -97,7 +120,8 @@ class _AboutScreenState extends State<AboutScreen> {
   }
 
   void _updateAboutBottomSheet(String about) {
-    final TextEditingController _controller = TextEditingController(text: about);
+    final TextEditingController _controller =
+        TextEditingController(text: about);
     showModalBottomSheet(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(
